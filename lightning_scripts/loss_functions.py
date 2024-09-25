f0ef8ddd210017237a14ee7721c4bef0a7bcdc1f
ThomasYerxa <tey214@nyu.edu>
2024-09-24 23:09:30 -0400

    added barlow loss

--- lightning_scripts/loss_functions.py
+++ lightning_scripts/loss_functions.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 class MMCR_Loss(nn.Module):
     def __init__(self, distributed=False):
         super().__init__()
-        self.distributed = distributed # currently a dummy variable for compat with original implementation
+        self.distributed = distributed 
     
     def forward(self, z1, z2):
         z1 = F.normalize(z1, dim=-1, p=2)
@@ -26,3 +26,38 @@ class MMCR_Loss(nn.Module):
             return torch.cat(tensor_list)
         else:
             return tensor
+
+
+class Barlow_Loss(nn.Module):
+    def __init__(self, lmbda: float, out_dim=8192, scale_loss: float = 0.048, distributed: bool = False):
+        super(Barlow_Loss, self).__init__()
+        self.lmbda = lmbda 
+        self.bn = nn.BatchNorm1d(out_dim, affine=False)
+        self.scale_loss = scale_loss
+        self.distributed = distributed
+
+    def forward(self, z1, z2):
+        # sum the cross-correlation matrix between all gpus
+        bs = z1.shape[0]
+        if self.distributed:
+            bs *= torch.distributed.get_world_size()
+
+        c_inv = self.bn(z1).T @ self.bn(z2)
+        c_inv.div_(bs)
+        if self.distributed:
+            torch.distributed.all_reduce(c_inv)
+
+        on_diag_inv = torch.diagonal(c_inv).add_(-1).pow_(2).sum()
+        off_diag_inv = off_diagonal(c_inv).pow_(2).sum()
+        loss = on_diag_inv + self.lmbda * off_diag_inv
+        loss = loss * self.scale_loss
+
+        return loss
+
+
+
+def off_diagonal(x):
+    # return a flattened view of the off-diagonal elements of a square matrix
+    n, m = x.shape
+    assert n == m
+    return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
